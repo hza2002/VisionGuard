f9c1a31d0590d35bcdabc4f8abf8279c604e9ec2
Bill Hu <hza2002@foxmail.com>
2024-04-10 18:34:09 +0800

    Add main app

---
+++ app.py
@@ -0,0 +1,25 @@
+import sys
+import qdarkstyle
+
+from PySide6 import QtGui
+from PySide6.QtWidgets import QMainWindow, QApplication
+
+from ui.vision_guard import Ui_MainWindow as VisionGuardMainWindow
+
+
+class VisionGuardApp(QMainWindow, VisionGuardMainWindow):
+
+    def __init__(self, parent=None):
+        super(VisionGuardApp, self).__init__(parent)
+        self.setupUi(self)
+
+    def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
+        super(VisionGuardApp, self).closeEvent(a0)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = VisionGuardApp()
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyside6())
+    window.show()
+    sys.exit(app.exec())

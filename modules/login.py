ec09b83b9a0320fa5140024894a416ea15eeba68
Bill Hu <hza2002@foxmail.com>
2024-04-10 20:34:54 +0800

    Add login module
    include account login and face login. face login contains xfyun liveness
    detection and face compare with verified face.

---
+++ modules/login.py
@@ -0,0 +1,70 @@
+import os
+import face_recognition
+import xfyun_liveness_detection as liveness_detection
+
+
+class Login:
+    def __init__(self) -> None:
+        self.__data_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "../resource/account.data"
+        )
+        self.__face_directory = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "../resource/verified_face/"
+        )
+        if not os.path.exists(self.__data_path):
+            with open(self.__data_path, "w") as file:
+                file.write("admin,admin\n")
+
+    def face_compare(self, img_path) -> bool:
+        files = os.listdir(self.__face_directory)
+        face_images = [
+            os.path.join(self.__face_directory, file)
+            for file in files
+            if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg")
+        ]
+        verified_faces = []
+        for image in face_images:
+            verified_face = face_recognition.load_image_file(image)
+            verified_face_encoding = face_recognition.face_encodings(verified_face)[0]
+            verified_faces.append(verified_face_encoding)
+
+        unknown_image = face_recognition.load_image_file(img_path)
+        unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+        results = face_recognition.compare_faces(verified_faces, unknown_encoding, 0.6)
+        if all(not item for item in results):  # 比对结果全为False
+            return False
+        return True
+
+    def face_login(self, img_path) -> bool:
+        # 静默活体检测
+        if not liveness_detection.run(img_path):
+            print("Liveness detection failed!")
+            return False
+        # 和已验证人脸数据进行比对
+        if not self.face_compare(img_path):
+            print("Face compare failed!")
+            return False
+        return True
+
+    def account_login(self, username, password) -> bool:
+        with open(self.__data_path, "r") as file:
+            accounts = file.readlines()
+            for account in accounts:
+                stored_username, stored_password = account.strip().split(",")
+                if stored_username == username and stored_password == password:
+                    return True
+        return False
+
+
+if __name__ == "__main__":
+    print(Login().account_login("admin", "admin"))
+    print(Login().account_login("hza", "tbw"))
+    know_img_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "../resource/know_face.jpeg"
+    )
+    unknow_img_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "../resource/unknow_face.jpg"
+    )
+    print(Login().face_login(know_img_path))
+    print(Login().face_login(unknow_img_path))
---
+++ modules/xfyun_liveness_detection.py
@@ -0,0 +1,143 @@
+# 静默活体检测 WebAPI 接口调用示例
+# 运行前：请先填写Appid、APIKey、APISecret以及图片路径
+# 运行方法：直接运行 main 即可
+# 结果： 控制台输出结果信息
+#
+# 接口文档（必看）：https://www.xfyun.cn/doc/face/xf-silent-in-vivo-detection/API.html
+
+from datetime import datetime
+from wsgiref.handlers import format_date_time
+from time import mktime
+import hashlib
+import base64
+import hmac
+from urllib.parse import urlencode
+import json
+import os
+import requests
+from dotenv import dotenv_values
+
+
+class AssembleHeaderException(Exception):
+    def __init__(self, msg):
+        self.message = msg
+
+
+class Url:
+    def __init__(this, host, path, schema):
+        this.host = host
+        this.path = path
+        this.schema = schema
+        pass
+
+
+# 进行sha256加密和base64编码
+def sha256base64(data):
+    sha256 = hashlib.sha256()
+    sha256.update(data)
+    digest = base64.b64encode(sha256.digest()).decode(encoding="utf-8")
+    return digest
+
+
+def parse_url(requset_url):
+    stidx = requset_url.index("://")
+    host = requset_url[stidx + 3 :]
+    schema = requset_url[: stidx + 3]
+    edidx = host.index("/")
+    if edidx <= 0:
+        raise AssembleHeaderException("invalid request url:" + requset_url)
+    path = host[edidx:]
+    host = host[:edidx]
+    u = Url(host, path, schema)
+    return u
+
+
+def assemble_ws_auth_url(requset_url, method="GET", api_key="", api_secret=""):
+    u = parse_url(requset_url)
+    host = u.host
+    path = u.path
+    now = datetime.now()
+    date = format_date_time(mktime(now.timetuple()))
+    signature_origin = "host: {}\ndate: {}\n{} {} HTTP/1.1".format(
+        host, date, method, path
+    )
+    # print(signature_origin)
+    signature_sha = hmac.new(
+        api_secret.encode("utf-8"),
+        signature_origin.encode("utf-8"),
+        digestmod=hashlib.sha256,
+    ).digest()
+    signature_sha = base64.b64encode(signature_sha).decode(encoding="utf-8")
+    authorization_origin = (
+        'api_key="%s", algorithm="%s", headers="%s", signature="%s"'
+        % (api_key, "hmac-sha256", "host date request-line", signature_sha)
+    )
+    authorization = base64.b64encode(authorization_origin.encode("utf-8")).decode(
+        encoding="utf-8"
+    )
+    values = {"host": host, "date": date, "authorization": authorization}
+
+    return requset_url + "?" + urlencode(values)
+
+
+def gen_body(appid, img_path, server_id):
+    with open(img_path, "rb") as f:
+        img_data = f.read()
+    body = {
+        "header": {"app_id": appid, "status": 3},
+        "parameter": {
+            server_id: {
+                "service_kind": "anti_spoof",
+                "anti_spoof_result": {
+                    "encoding": "utf8",
+                    "compress": "raw",
+                    "format": "json",
+                },
+            }
+        },
+        "payload": {
+            "input1": {
+                "encoding": "jpg",
+                "status": 3,
+                "image": str(base64.b64encode(img_data), "utf-8"),
+            }
+        },
+    }
+    return json.dumps(body)
+
+
+def run(img_path, server_id="s67c9c78c") -> bool:
+    env_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "../resource/.env"
+    )
+    cfg = dotenv_values(env_path)
+    appid, api_secret, api_key = (
+        cfg["LIVENESS_DETECTION_APPID"],
+        cfg["LIVENESS_DETECTION_APISECRET"],
+        cfg["LIVENESS_DETECTION_APIKEY"],
+    )
+
+    url = "http://api.xf-yun.com/v1/private/{}".format(server_id)
+    request_url = assemble_ws_auth_url(url, "POST", api_key, api_secret)
+    headers = {
+        "content-type": "application/json",
+        "host": "api.xf-yun.com",
+        "app_id": appid,
+    }
+    response = requests.post(
+        request_url, data=gen_body(appid, img_path, server_id), headers=headers
+    )
+    resp_data = json.loads(response.content.decode("utf-8"))
+    decode_json = json.loads(
+        base64.b64decode(resp_data["payload"]["anti_spoof_result"]["text"]).decode()
+    )
+    print(decode_json)
+    return decode_json["passed"]
+
+
+if __name__ == "__main__":
+    test_img_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "../resource/test/"
+    )
+    print(run(os.path.join(test_img_path, "face_fake.jpg")))
+    print(run(os.path.join(test_img_path, "face_real.jpg")))

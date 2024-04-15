a7c20900e5e9cbeae2f963389e1d39dc29a0c831
Soup-Bibo <tbw760013416@outlook.com>
2024-04-15 16:13:24 +0800

    Rename email

--- utils/alarm_email.py
+++ utils/alarm_email.py
@@ -1,6 +1,8 @@
 import smtplib
+import cv2
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 
 from utils.config import Config
 
@@ -22,7 +24,7 @@ class Email:
     # def __del__(self):
     #     self.server.quit()
 
-    def send_email(self, content, subject="Security Alert"):
+    def send_email(self, content, image=None, subject="Security Alert"):
         message = MIMEMultipart()
         message["From"] = self.from_email
         message["To"] = self.to_email
@@ -30,6 +32,13 @@ class Email:
         # Add in the message body
         message_body = str(content)
         message.attach(MIMEText(message_body, "plain"))
+
+        if image is not None:
+            _, img_encoded = cv2.imencode('.jpg', image)
+            img_attachment = MIMEImage(img_encoded.tostring())
+            img_attachment.add_header('Content-Disposition', 'attachment; filename=img.jpg')
+            message.attach(img_attachment)
+
         self.server.sendmail(self.from_email, self.to_email, message.as_string())
 
 
